dab2ce0d3b8f7edbc1d0b416208490ea7fb926b4
mahsa <mahsa.jannaty@gmail.com>
2023-04-20 18:29:53 +0330

    login view and route

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
 
-# Register your models here.
+# Signup.html your models here.
--- redirect/admin.py
+++ redirect/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-# Register your models here.
+# Signup.html your models here.
 from .models import ShortenedLink
+from .models import ShortLink
+from .models import FullLink
 
 admin.site.register(ShortenedLink)
+admin.site.register(ShortLink)
+admin.site.register(FullLink)
